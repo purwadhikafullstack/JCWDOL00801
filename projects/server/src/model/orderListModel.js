1b5c231d57f0c30b5437f2689cd0c32deaee0339
danielsebastianc <danielsebastian207@gmail.com>
2023-05-11 20:57:27 +0700

    [PRA-Bug-Daniel] Fixing pagination room and property list layout, fix sales report, fix calendar report layout, fix order status, fix db models

--- projects/server/src/model/orderListModel.js
+++ projects/server/src/model/orderListModel.js
@@ -1,33 +1,33 @@
-const {Sequelize} = require('sequelize');
-const { dbSequelize } = require('../config/db');
-const {DataTypes} = Sequelize;
+const { Sequelize } = require("sequelize");
+const { dbSequelize } = require("../config/db");
+const { DataTypes } = Sequelize;
 
-const orderListModel = dbSequelize.define("orderLists", {
-    orderId: {
-        primaryKey: true,
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        allowNull: false
-    },
-    //fk
-    roomId: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    //fk
-    transactionId: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    //fk
-    guestId: {
-        type: DataTypes.INTEGER,
-        allowNull: true
-    },
-    price: {
-        type: DataTypes.DECIMAL(10, 2),
-        allowNull: false
-    }
-})
+const orderListModel = dbSequelize.define("orderlists", {
+  orderId: {
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  //fk
+  roomId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  //fk
+  transactionId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  //fk
+  guestId: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+});
 
-module.exports = {orderListModel}
+module.exports = { orderListModel };
